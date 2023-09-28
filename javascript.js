12f34af8b0301f82a43ecccb7354807db780f610
Tatum Baldwin <tjbaldwin@students.nic.edu>
2023-09-28 15:40:20 -0700

    Finished

--- javascript.js
+++ javascript.js
@@ -1,13 +1,60 @@
 // 1.1 Assignment: Where do you put JavaScript?
+
 alert('Javascript is easy')
+
 // 1.2 Assignment: Variables
+
+var mansName = "john";
+console.log(mansName);
 
 // 1.3 Assignment: Variable Declaration & Initialization
 
+var z = "Donkey";
+var y = 1;
+var x = false;
+var w = undefined;
+var v = null;
+
+console.log(z, y, x, w, v);
+
 // 1.4 Assignment: Complete your statements
+
+x = 2;
+y = 3;
+
+z = x + y;
 
 // 1.5 Assignment: Brackets, Braces and Parentheses
 
+var a = function(){
+ alert('Statement 1');
+ alert('Statement 2');
+}
+
+console.log(a());
+
+
+
 // 1.6 Assignment: Data Types
 
+
+var a = "name";  //string
+var b = 22;      //number
+var c = true ;   //boolean
+var d = null;   //null
+var e = undefined;
+
+
+
+console.log(typeof(a))
+console.log(typeof(b))
+console.log(typeof(c))
+console.log(typeof(d))
+
 // 1.7 Assignment: Practicing JavaScript basics
+
+var a = 4;
+var b = 3;
+var c = 5;
+
+console.log(a * b / c)
